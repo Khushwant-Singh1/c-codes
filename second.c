994989ffce4fa03442c2ae2e9842fad01ce88d91
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-01-03 21:48:51 +0530

    C codes

---
+++ second.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a, b;
+    printf("Enter a number a: \n");
+    scanf("%d", &a);
+
+    printf("Enter a number b:\n");
+    scanf("%d", &b);
+
+    return 0;
+}
