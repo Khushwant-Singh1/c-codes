d5830ffac753e5eea54ee3accbbf7a1c4d2f8430
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-01-20 04:46:03 +0530

    C codes

---
+++ conditions.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d" , &age);
+    printf("You have entered %d as your age\n", age);
+    if(age>=18){
+        printf("you are eligible to vote!");
+    }
+    else if(age>10){
+        printf("Your age is between 10 and 18!!");
+
+    }
+    else{ printf("yor cannot vote");
+
+    }
+}
