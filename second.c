2eaeadef7554f96733110c42d1fe325579898233
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-01-19 14:23:42 +0530

    C codes

--- second.c
+++ second.c
@@ -5,12 +5,8 @@ int main()
     int a, b;
     printf("Enter a number a: \n");
     scanf("%d", &a);
-
     printf("Enter a number b:\n");
     scanf("%d", &b);
-    
-
-    printf("The sum is : %d\n", a+b);
-    
+    printf("The sum is : %d\n", a + b);
     return 0;
 }
