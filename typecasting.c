f0746e64dde37dd4cf25930975d66736197681a5
Khushwant-Singh1 <khushwantzx@gmail.com>
2024-01-31 19:26:30 +0530

    C codes

---
+++ typecasting.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+// Typecasting syntax
+// (type) value;
+int main(){
+    int a = 3;
+    float b = (float)54/5;
+    printf("The value of a is %f\n", (float) a);
+    printf("The value of b is %f\n", b);
+
+
+    printf("The value of b is %d\n", (int)b);
+    return 0;
+}
